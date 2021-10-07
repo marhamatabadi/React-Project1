c957f054f076a93494d119cb087ab76d94c805a9
alireza marhamatabadi <marhamatabadi@outlook.com>
2021-10-07 05:02:47 +0300

    using classes insted of functional components

--- src/components/header.js
+++ src/components/header.js
@@ -1,12 +1,24 @@
-import React from "react";
+import React,{Component} from "react";
 
-const getYear = () => {
-    const now = new Date();
-    return now.getFullYear();
-}
-const Header = () => {
-    return (
-        <div>this year is {getYear()}</div>
-    )
+class Header extends Component{
+ 
+    render(){
+        let styles={
+            header:{
+                background:'#03a9f4'
+            },
+            logo:{
+                color:'#fff',
+                fontFamily:'Anton',
+                textAlign:'center'
+            }
+        }
+        return (
+            <header style={styles.header}>
+                <div style={styles.logo}>Logo</div>
+                <input/>
+            </header>
+        )
+    }
 }
 export default Header;
