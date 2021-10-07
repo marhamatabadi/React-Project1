30d6db5ef2c63c36971f9fac332df501aa19206a
alireza marhamatabadi <marhamatabadi@outlook.com>
2021-10-07 05:23:44 +0300

    styling intro

--- src/components/header.js
+++ src/components/header.js
@@ -3,19 +3,9 @@ import React,{Component} from "react";
 class Header extends Component{
  
     render(){
-        let styles={
-            header:{
-                background:'#03a9f4'
-            },
-            logo:{
-                color:'#fff',
-                fontFamily:'Anton',
-                textAlign:'center'
-            }
-        }
         return (
-            <header style={styles.header}>
-                <div style={styles.logo}>Logo</div>
+            <header>
+                <div className="logo">Logo</div>
                 <input/>
             </header>
         )
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
+import './styles/style.css'
 import Header from './components/header'
 import News from './components/news'
 
